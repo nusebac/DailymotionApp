1741a07b503c913f59609d005b894ec52997a0c3
Arka Gupta <a0120039@nus.edu.sg>
2015-02-02 15:26:39 +0800

    Dailymotion App Updated Changes

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -89,26 +89,7 @@ def home(request):
 
 
 def common(request):
-    for data in rows:
-        tech_id.append(data[0])
-        trend_name.append(data[1])
-        trend_desc.append(data[2])
-        trend_lang.append(data[3])
-        trend_date.append(data[4])
-        trend_create.append(data[5])
-        trend_update.append(data[6])
-        trend_upid.append(data[7])
-        trend_score.append(data[8])
-        trend_temporality.append(data[9])
-        trend_sex.append(data[10])
-        trend_age.append(data[11])
-        trend_region.append(data[12])
-        trend_related.append(data[13])
-        trend_external.append(data[14])
-        trend_source.append(data[15])
-        trend_topic.append(data[16])
-
-    content = Context({"Data": similarity,'Tech_ID': tech_id,'trend_name': trend_name, "trend_desc":trend_desc,"trend_lang": trend_lang,"Created_Date": trend_create,"Updated_Date": trend_update,"trend_score": trend_score,"trend_temporality": trend_temporality,"trend_Sex": trend_sex,"trend_age": trend_age,"trend_region": trend_region,"trend_related": trend_related,"trend_external": trend_external,"trend_source": trend_source,"trend_topic": trend_topic})
+    content = Context({"Data": rows})
     template = "common.html"
     return render(request,template,content)
 
